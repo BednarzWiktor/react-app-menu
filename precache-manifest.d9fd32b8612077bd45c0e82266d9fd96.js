29671b231b244b8ca5ed52d94ce4ccf8988cb960
Wiktor Bednarz <b.w.roboczy@gmail.com>
2019-03-14 11:45:39 +0100

    Updates

---
+++ precache-manifest.d9fd32b8612077bd45c0e82266d9fd96.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "86e91a38152576151e4e2178c76b37f6",
+    "url": "/react-app-menu/static/media/cocktail2.86e91a38.svg"
+  },
+  {
+    "revision": "71a20f8e71714e0bed7f",
+    "url": "/react-app-menu/static/css/main.e25dd35c.chunk.css"
+  },
+  {
+    "revision": "8fce7ab0bf3da8f9e36c",
+    "url": "/react-app-menu/static/js/runtime~main.f0f260b4.js"
+  },
+  {
+    "revision": "19fcc13b7b032c210b89",
+    "url": "/react-app-menu/static/js/2.facdb513.chunk.js"
+  },
+  {
+    "revision": "51b14a505f2c9da850b9b6447b8a62bb",
+    "url": "/react-app-menu/static/media/wino1.51b14a50.svg"
+  },
+  {
+    "revision": "2ff0fa4c75c2498601622a274d35e5d9",
+    "url": "/react-app-menu/static/media/wino1a.2ff0fa4c.svg"
+  },
+  {
+    "revision": "0cf3ed37b5abddff0f978feef6f4d081",
+    "url": "/react-app-menu/static/media/bg1.0cf3ed37.jpg"
+  },
+  {
+    "revision": "6831b3450d4ec8a5535902b57b96797a",
+    "url": "/react-app-menu/static/media/Amarante-Regular.6831b345.woff"
+  },
+  {
+    "revision": "e7379eb858e616a763b160c1ed99c718",
+    "url": "/react-app-menu/static/media/Amarante-Regular.e7379eb8.otf"
+  },
+  {
+    "revision": "378bff3c13874473de9bf51997fce627",
+    "url": "/react-app-menu/static/media/MinionPro-Regular.378bff3c.woff"
+  },
+  {
+    "revision": "7c788b44d11f39dd0f368e94c9665c70",
+    "url": "/react-app-menu/static/media/Amarante-Regular.7c788b44.ttf"
+  },
+  {
+    "revision": "f534b25ade73568102cb1775f722d4f5",
+    "url": "/react-app-menu/static/media/MinionPro-Regular.f534b25a.otf"
+  },
+  {
+    "revision": "574626939126f6227cf33029db863b9f",
+    "url": "/react-app-menu/static/media/MinionPro-Regular.57462693.ttf"
+  },
+  {
+    "revision": "732841d221dc05f10e24de66c490ca1b",
+    "url": "/react-app-menu/static/media/cocktail1.732841d2.svg"
+  },
+  {
+    "revision": "71a20f8e71714e0bed7f",
+    "url": "/react-app-menu/static/js/main.d75e4597.chunk.js"
+  },
+  {
+    "revision": "9c05b316a7f5c60928d3d3b43006a633",
+    "url": "/react-app-menu/static/media/cocktail3.9c05b316.svg"
+  },
+  {
+    "revision": "718fd52df48d7d9749da88124c872974",
+    "url": "/react-app-menu/static/media/cocktail4.718fd52d.svg"
+  },
+  {
+    "revision": "180a6d7524c5ff170f4f5d603f4d8cfe",
+    "url": "/react-app-menu/static/media/piwo1.180a6d75.svg"
+  },
+  {
+    "revision": "c9591cedf3fff31d6d3ae95a0406d06f",
+    "url": "/react-app-menu/static/media/piwo2.c9591ced.svg"
+  },
+  {
+    "revision": "98f6f4896b7e475922216c58bb3d1379",
+    "url": "/react-app-menu/static/media/piwo1a.98f6f489.svg"
+  },
+  {
+    "revision": "6d02a4d328e4ff19a895adb2004d0bbf",
+    "url": "/react-app-menu/static/media/piwo2a.6d02a4d3.svg"
+  },
+  {
+    "revision": "aae29b5f26f27c2271c132c2a33f8af5",
+    "url": "/react-app-menu/static/media/top-deco.aae29b5f.svg"
+  },
+  {
+    "revision": "d6d360b33e83863cf3bdad1ca0eaaedb",
+    "url": "/react-app-menu/static/media/card-deco2.d6d360b3.svg"
+  },
+  {
+    "revision": "426e51a9c6a24d22365090fc5099b3f3",
+    "url": "/react-app-menu/static/media/card-side-left.426e51a9.svg"
+  },
+  {
+    "revision": "c3967930848171110d0eac091ec16b8a",
+    "url": "/react-app-menu/static/media/card-side-right.c3967930.svg"
+  },
+  {
+    "revision": "b9065dd24ebbd2ca283a1b5725258b96",
+    "url": "/react-app-menu/static/media/card-side-right2.b9065dd2.svg"
+  },
+  {
+    "revision": "0f30cfc955478832ab8253b4845ca4a1",
+    "url": "/react-app-menu/static/media/card-side-left2.0f30cfc9.svg"
+  },
+  {
+    "revision": "e8dda19b3ca4ed2ebfa556adfd500a72",
+    "url": "/react-app-menu/index.html"
+  }
+];
